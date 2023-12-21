453f21166b695b6a0cd109d328adfbb42527602e
mariapia060608 <ing.lucasmoreno@gmail.com>
2023-12-21 13:59:58 -0300

    Cambio de PC

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { InicializacionComponent } from './inicializacion/inicializacion.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard/home' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'init', component: InicializacionComponent },
   { path: '**', component: ErrorPageComponent },
 ];
