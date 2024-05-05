d481144fa9326d0b2f6c87df727694470aef64f2
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2024-05-05 18:25:42 -0300

    Actualizacion Mayo

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,17 +3,31 @@ import { AuthService } from '../../services/auth.service';
 import { DataService } from '../../services/data.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import NavbarSeccionesComponent from './components/navbar-secciones/navbar-secciones.component';
+import NavbarItemComponent from './components/navbar-item/navbar-item.component';
 
 @Component({
   standalone: true,
   selector: 'app-navbar',
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NavbarSeccionesComponent,
+    NavbarItemComponent
+  ],
   templateUrl: './navbar.component.html',
   styleUrls: []
 })
 export class NavbarComponent implements OnInit {
 
   public usuarioLogin: any = null;
+
+  public configuraciones = {
+    descripcion: 'Configuraciones',
+    items: [
+      { nombre: 'Usuarios', ruta: 'usuarios' },
+    ]
+  }
 
   constructor(
     public authService: AuthService,
